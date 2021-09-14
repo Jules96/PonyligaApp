c0bde30e8b0198a033eab92ce9f33461297c718f
Michael Niers <michael.niers@ewetel.net>
2021-09-14 10:13:59 +0200

    Create

--- Ponyliga/Ponyliga/Views/UserEditingPage.xaml.cs
+++ Ponyliga/Ponyliga/Views/UserEditingPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Ponyliga.Models;
+using Ponyliga.Services;
 
 namespace Ponyliga.Views
 {
@@ -15,6 +17,45 @@ namespace Ponyliga.Views
         public UserEditingPage()
         {
             InitializeComponent();
+
+            userRightsPicker.Items.Add("Admin");
+            userRightsPicker.Items.Add("User");
+            /*userRightsPicker.Items.Add("Rechte 3");
+            userRightsPicker.Items.Add("Rechte 4");*/
+        }
+
+        private void btn_newUser_Clicked(object sender, EventArgs e)
+        {
+            string firstName = userFirstName.Text;
+            string lastName = userLastName.Text;
+            int rights = userRightsPicker.SelectedIndex;
+
+
+            string loginName = userLoginName.Text;
+            string password = userPassword.Text;
+
+            //Liste
+
+
+            if (rights != -1)
+            {
+                User user = new User();
+                user.id = default;
+                user.firstName = firstName;
+                user.loginName = loginName;
+                user.surName = lastName;
+                user.passwordHash = password;
+                user.userPrivileges = rights;
+
+                ApiService apiService = new ApiService();
+                apiService.UpdateUser(user.id, user);
+
+                Navigation.PushAsync(new MainPageAfterLogin());
+            }
+            else
+            {
+                DisplayAlert("Fehler", "Es wurden nicht alle Felder ausgefüllt!", "OK");
+            }
         }
     }
 }
