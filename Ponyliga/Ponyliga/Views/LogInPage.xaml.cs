b23421af83056f8b485d80ce2f05fa3a7650eb0d
fabi503 <75164780+fabi503@users.noreply.github.com>
2021-09-21 10:15:27 +0200

    first changes

--- Ponyliga/Ponyliga/Views/LogInPage.xaml.cs
+++ Ponyliga/Ponyliga/Views/LogInPage.xaml.cs
@@ -22,50 +22,33 @@ namespace Ponyliga.Views
 
         private void btn_LogIn_Clicked(object sender, EventArgs e)
         {
-            User user = new User();
-            user.id = default;
-            user.firstName = default;
-            user.surName = default;
-            user.loginName = username.Text;
-            user.passwordHash = password.Text;
-            user.userPrivileges = default;
+            //User user = new User();
+            //user.id = default;
+            //user.firstName = default;
+            //user.surName = default;
+            //user.loginName = username.Text;
+            //user.passwordHash = password.Text;
+            //user.userPrivileges = default;
 
 
-            ApiService apiService = new ApiService();
-            bool result = apiService.LogInUser(user).Result;
+            //ApiService apiService = new ApiService();
+            //bool result = apiService.LogInUser(user).Result;
 
 
-            if (result)
-            {
-                Navigation.PushAsync(new MainPageAfterLogin());
-            }
-            else
-            {
-                DisplayAlert("Fehler", "Passwort oder Benutzername ist falsch!", "ok");
-            }
+            //if (result)
+            //{
+            //    Navigation.PushAsync(new MainPageAfterLogin());
+            //}
+            //else
+            //{
+            //    DisplayAlert("Fehler", "Passwort oder Benutzername ist falsch!", "ok");
+            //}
+            Navigation.PushAsync(new MainPageAfterLogin());
         }
 
         private void btn_Register_Clicked(object sender, EventArgs e)
         {
-            User user = new User();
-            user.id = default;
-            user.firstName = default;
-            user.surName = default;
-            user.loginName = username.Text;
-            user.passwordHash = password.Text;
-            user.userPrivileges = default;
-
-
-            ApiService apiService = new ApiService();
-            bool result = apiService.LogInUser(user).Result;
-
-            
-            if(result)
-            {
-                Navigation.PushAsync(new CreateUserPage());
-            }
-
-            //Navigation.PushAsync(new CreateUserPage());
+            Navigation.PushAsync(new CreateUserPage());
         }
 
         private void btn_MainPage_Clicked(object sender, EventArgs e)
