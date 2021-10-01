b52106f711f4fd1f65d8a2d332a970bd4ba80c03
Julia <Julia@DESKTOP-GT1MDLG>
2021-10-01 10:12:39 +0200

    xd

---
+++ Ponyliga.Test/Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Ponyliga.Views;
+
+namespace PonyLiga.Test
+{
+    [TestFixture(Platform.Android)]
+    
+    public class Tests
+    {
+       IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void UserPageFillList()
+        {
+            //Arrange
+
+            //Act
+            UserPage userPage = new UserPage();
+            userPage.FillUserList();
+            //Assert
+            //AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+            //AppResult[] results = app.
+            app.Screenshot("Filled User Table");
+
+            //Assert.IsTrue(results.Any());
+        }
+    }
+}
